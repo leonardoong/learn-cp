0f67b37511d733c5daf5dff709156c4d71c99fa2
leonardoong <leonardo.ong19@gmail.com>
2022-04-26 00:03:21 +0700

    palindrome number

---
+++ leetcode/palindrome_number/palindrome.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 121
+	fmt.Println(isPalindrome(x))
+
+	x2 := 123
+	fmt.Println(isPalindrome(x2))
+
+	x3 := -1232132
+	fmt.Println(isPalindrome(x3))
+}
+
+func isPalindrome(x int) bool {
+	if x <= 0 {
+		return false
+	}
+
+	ori := x
+	rev := 0
+
+	for ori != 0 {
+		rev = rev*10 + ori%10
+		ori = ori / 10
+	}
+
+	return x == rev
+}
