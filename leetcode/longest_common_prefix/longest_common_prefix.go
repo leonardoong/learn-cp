b60b3f6fbeebfca161195cda00b22ef16d30dcb4
leonardoong <leonardo.ong19@gmail.com>
2022-04-27 01:29:56 +0700

    longest common prefix

---
+++ leetcode/longest_common_prefix/longest_common_prefix.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+	fmt.Println(longestCommonPrefix([]string{"dog", "racecar", "car"}))
+}
+
+func longestCommonPrefix(strs []string) string {
+	res := ""
+
+	for i := 0; i < len(strs[0]); i++ {
+		for j := 1; j < len(strs); j++ {
+			if i >= len(strs[j]) || strs[j][i] != strs[0][i] {
+				return res
+			}
+		}
+
+		res += string(strs[0][i])
+	}
+
+	return res
+}
