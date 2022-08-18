2a30c6fac872afbb552a70c1e426921657d2e246
leonardoong <leonardo.ong19@gmail.com>
2022-08-18 19:52:31 +0700

    add comments three sum

--- leetcode/three_sum/three_sum.go
+++ leetcode/three_sum/three_sum.go
@@ -11,18 +11,23 @@ func main() {
 
 func threeSum(nums []int) [][]int {
 
+	// 1. sort the array
 	sort.Slice(nums, func(i, j int) bool {
 		return nums[i] < nums[j]
 	})
 
 	res := [][]int{}
 
+	// 2. loop through the array
 	for i, n := range nums {
+		// 3. if the current number is the same as the previous number, skip it
 		if i > 0 && n == nums[i-1] {
 			continue
 		}
 
+		// 4. find the target number
 		l, r := i+1, len(nums)-1
+		// 5. use two sums with 2 pointers to find the target number
 		for l < r {
 			threeSum := n + nums[l] + nums[r]
 			if threeSum > 0 {
@@ -31,6 +36,7 @@ func threeSum(nums []int) [][]int {
 				l++
 			} else {
 				res = append(res, []int{n, nums[l], nums[r]})
+				// 6. skip the duplicates
 				l++
 				for nums[l] == nums[l-1] {
 					if l < r {
