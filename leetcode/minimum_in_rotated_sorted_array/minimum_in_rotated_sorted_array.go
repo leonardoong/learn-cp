3c2b7d5b81d1fcfaa56eff38dbec33f3565212fe
leonardoong <leonardo.ong19@gmail.com>
2022-09-06 00:44:53 +0700

    mininmum in rotated sorted array

---
+++ leetcode/minimum_in_rotated_sorted_array/minimum_in_rotated_sorted_array.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(findMin([]int{4, 5, 6, 7, 0, 1, 2}))
+	fmt.Println(findMin([]int{5, 1, 2, 3, 4}))
+	fmt.Println(findMin([]int{11, 13, 15, 17}))
+}
+
+func findMin(nums []int) int {
+	l, r := 0, len(nums)-1
+	min := nums[0]
+	for l <= r {
+		m := (l + r) / 2
+
+		if nums[m] < min {
+			min = nums[m]
+		}
+
+		if nums[r] < nums[l] && nums[m] > nums[r] {
+			l = m + 1
+		} else {
+			r = m - 1
+		}
+	}
+
+	return min
+}
