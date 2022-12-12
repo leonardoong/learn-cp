75cd69532d7d3f72d8b266328b8cb51ab8b948de
leonardoong <leonardo.ong19@gmail.com>
2022-12-13 02:02:50 +0700

    add stack solution

--- leetcode/next_greater_element_2/next_greater_element_2.go
+++ leetcode/next_greater_element_2/next_greater_element_2.go
@@ -10,6 +10,9 @@ func main() {
 	fmt.Println(nextGreaterElements2([]int{1, 2, 1}))
 	fmt.Println(nextGreaterElements2([]int{1, 2, 3, 4, 3}))
 
+	fmt.Println(nextGreaterElements2([]int{1, 2, 1}))
+	fmt.Println(nextGreaterElements3([]int{1, 2, 3, 4, 3}))
+
 }
 
 // brute force 1
@@ -52,4 +55,32 @@ func nextGreaterElements2(nums []int) []int {
 	}
 
 	return result
+}
+
+// using stack
+// TC : O(n)
+// SC : O(n)
+type node struct {
+	index int
+	value int
+}
+
+func nextGreaterElements3(nums []int) []int {
+	length := len(nums)
+	// double the slice to loop twice for circularity
+	nums = append(nums, nums...)
+
+	var stack []node
+
+	for i := 0; i < len(nums); i++ {
+		for len(stack) > 0 && stack[len(stack)-1].value < nums[i] {
+			node := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			nums[node.index] = nums[i]
+		}
+		stack = append(stack, node{i, nums[i]})
+		nums[i] = -1
+	}
+
+	return nums[:length]
 }
