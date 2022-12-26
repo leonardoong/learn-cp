2f2a64d220d5405b95df9d6853ab064253ff7532
leonardoong <leonardo.ong19@gmail.com>
2022-12-26 23:40:26 +0700

    product aray except self

---
+++ leetcode/product_array_except_self/product_array_except_self.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(productExceptSelf([]int{1, 2, 3, 4}))
+	fmt.Println(productExceptSelf([]int{-1, 1, 0, -3, 3}))
+}
+
+func productExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+
+	prefix := 1
+	for i := 0; i < len(nums); i++ {
+		res[i] = prefix
+		prefix *= nums[i]
+	}
+
+	postfix := 1
+	for j := len(nums) - 1; j >= 0; j-- {
+		res[j] *= postfix
+		postfix *= nums[j]
+	}
+
+	return res
+}
