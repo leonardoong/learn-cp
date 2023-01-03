a112cd2917f97973f05292f6410584861d8bf5e3
leonardoong <leonardo.ong19@gmail.com>
2023-01-03 19:15:43 +0700

    even odds

---
+++ codeforces/problemset/318A.EvenOdds/even_odds.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long k, n;
+    cin >> n >> k;
+
+    if (k <= (n + 1) / 2)
+    {
+        cout << k * 2 - 1 << endl;
+    }
+    else
+    {
+        cout << (k - (n + 1) / 2) * 2 << endl;
+    }
+}
