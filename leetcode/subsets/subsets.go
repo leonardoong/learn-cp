c9ec4de6fea37754e163c2c518df875d2189d617
leonardoong <leonardo.ong19@gmail.com>
2023-02-16 22:07:51 +0700

    subsets

---
+++ leetcode/subsets/subsets.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(subsets([]int{1, 2, 3}))
+}
+
+func subsets(nums []int) [][]int {
+	var queue [][]int
+	queue = append(queue, []int{})
+
+	for i := 0; i < len(nums); i++ {
+		length := len(queue)
+		fmt.Println(length)
+
+		for j := 0; j < length; j++ {
+			item := queue[j]
+
+			tmp := make([]int, len(item))
+			copy(tmp, item)
+			tmp = append(tmp, nums[i])
+
+			queue = append(queue, tmp)
+		}
+	}
+
+	return queue
+}
