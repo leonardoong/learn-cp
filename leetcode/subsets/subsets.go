23d4389eb05e62e4b7fb65ddf9292627bc8dd8ec
leonardoong <leonardo.ong19@gmail.com>
2023-02-16 22:19:16 +0700

    turns out temp is necessary

--- leetcode/subsets/subsets.go
+++ leetcode/subsets/subsets.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(subsets([]int{1, 2, 3}))
+	fmt.Println(subsets([]int{9, 0, 3, 5, 7}))
 }
 
 func subsets(nums []int) [][]int {
@@ -12,11 +12,12 @@ func subsets(nums []int) [][]int {
 
 	for i := 0; i < len(nums); i++ {
 		length := len(queue)
-		fmt.Println(length)
 
 		for j := 0; j < length; j++ {
 			item := queue[j]
 
+			tmp := make([]int, len(item))
+			copy(tmp, item)
 			item = append(item, nums[i])
 			queue = append(queue, item)
 		}
