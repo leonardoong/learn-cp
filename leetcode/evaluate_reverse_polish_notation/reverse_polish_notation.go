9bd65a51da1ad176bddc34d5f849cc00ea352d04
leonardoong <leonardo.ong19@gmail.com>
2023-02-19 15:36:53 +0700

    evaluate reverse polish notation

---
+++ leetcode/evaluate_reverse_polish_notation/reverse_polish_notation.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(evalRPN([]string{"2", "1", "+", "3", "*"}))
+	fmt.Println(evalRPN([]string{"4", "13", "5", "/", "+"}))
+	fmt.Println(evalRPN([]string{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}))
+}
+
+func evalRPN(tokens []string) int {
+	stack := []int{}
+
+	for _, v := range tokens {
+		if v == "+" {
+			a := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			b := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, a+b)
+		} else if v == "-" {
+			a := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			b := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, b-a)
+		} else if v == "*" {
+			a := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			b := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, b*a)
+		} else if v == "/" {
+			a := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			b := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, b/a)
+		} else {
+			vInt, _ := strconv.Atoi(v)
+			stack = append(stack, vInt)
+		}
+	}
+
+	return stack[0]
+}
