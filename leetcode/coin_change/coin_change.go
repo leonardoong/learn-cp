99ab7c3cc87e27ee67063f476abad47b676d25c9
leonardoong <leonardo.ong19@gmail.com>
2023-10-03 21:45:29 +0700

    coin change

---
+++ leetcode/coin_change/coin_change.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(coinChange([]int{1, 2, 5}, 11))
+	fmt.Println(coinChange([]int{2}, 3))
+	fmt.Println(coinChange([]int{2}, 0))
+}
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	for i := range dp {
+		dp[i] = math.MaxInt32
+	}
+	dp[0] = 0
+
+	for a := 1; a <= amount; a++ {
+		for _, c := range coins {
+			if a-c < 0 {
+				continue
+			}
+
+			dp[a] = min(dp[a-c]+1, dp[a])
+		}
+	}
+
+	if dp[amount] == math.MaxInt32 {
+		return -1
+	}
+	return dp[amount]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
